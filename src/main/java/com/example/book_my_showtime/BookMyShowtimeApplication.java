fd027e42d8a2258179a00940ab781115c4ff8f4c
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-05-28 15:15:48 +0000

    Initial commit

---
+++ src/main/java/com/example/book_my_showtime/BookMyShowtimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_my_showtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_my_showtime/BookMyShowtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_my_showtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
